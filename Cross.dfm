7ef3430c642f9a08897584d8bb4d935a7a551aa6
Dmitry Yaskovich <kadavr95@outlook.com>
2022-08-01 22:43:23 +0300

    Add different app modes

--- Cross.dfm
+++ Cross.dfm
@@ -1,17 +1,19 @@
 object App: TApp
   Left = 0
   Top = 0
-  Caption = 'Cross v0.1'
-  ClientHeight = 299
-  ClientWidth = 635
+  Caption = 'Cross'
+  ClientHeight = 500
+  ClientWidth = 500
   Color = clFuchsia
   TransparentColor = True
   TransparentColorValue = clFuchsia
+  DoubleBuffered = True
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  Menu = AppMenu
   OldCreateOrder = False
   OnCreate = FormCreate
   OnMouseWheelDown = FormMouseWheelDown
@@ -20,4 +22,40 @@ object App: TApp
   OnResize = FormResize
   PixelsPerInch = 96
   TextHeight = 13
+  object AppMenu: TMainMenu
+    object Mode1: TMenuItem
+      Caption = 'Mode'
+      object Rectangle1: TMenuItem
+        Caption = 'Rectangle'
+        Checked = True
+        RadioItem = True
+        OnClick = Rectangle1Click
+      end
+      object Circle1: TMenuItem
+        Caption = 'Circle'
+        RadioItem = True
+        OnClick = Circle1Click
+      end
+    end
+    object Window1: TMenuItem
+      Caption = 'Window'
+      object Square1: TMenuItem
+        Caption = 'Square'
+        RadioItem = True
+        OnClick = Square1Click
+      end
+      object Custom1: TMenuItem
+        Caption = 'Custom'
+        Checked = True
+        RadioItem = True
+        OnClick = Custom1Click
+      end
+    end
+    object Help: TMenuItem
+      Caption = 'Help'
+      object About1: TMenuItem
+        Caption = 'About'
+      end
+    end
+  end
 end
--- Cross.pas
+++ Cross.pas
@@ -3,12 +3,23 @@ unit Cross;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Menus;
 
 type
   TApp = class(TForm)
-    procedure DrawCross;
+    AppMenu: TMainMenu;
+    Mode1: TMenuItem;
+    Rectangle1: TMenuItem;
+    Circle1: TMenuItem;
+    Help: TMenuItem;
+    About1: TMenuItem;
+    Window1: TMenuItem;
+    Square1: TMenuItem;
+    Custom1: TMenuItem;
+    procedure DrawRectangle;
+    procedure DrawCircle;
     procedure FormPaint(Sender: TObject);
     procedure FormResize(Sender: TObject);
     procedure FormMouseWheelDown(Sender: TObject; Shift: TShiftState;
@@ -16,8 +27,13 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormMouseWheelUp(Sender: TObject; Shift: TShiftState;
       MousePos: TPoint; var Handled: Boolean);
+    procedure Circle1Click(Sender: TObject);
+    procedure Rectangle1Click(Sender: TObject);
+    procedure Custom1Click(Sender: TObject);
+    procedure Square1Click(Sender: TObject);
   private
     size: integer;
+    appMode, resizingMode: string;
   public
     { Public declarations }
   end;
@@ -29,7 +45,40 @@ implementation
 
 {$R *.dfm}
 
-procedure TApp.DrawCross();
+procedure TApp.Rectangle1Click(Sender: TObject);
+begin
+  Rectangle1.Checked := True;
+  Circle1.Checked := False;
+  appMode := 'Rectangle';
+  DrawRectangle();
+end;
+
+procedure TApp.Circle1Click(Sender: TObject);
+begin
+  Circle1.Checked := True;
+  Rectangle1.Checked := False;
+  appMode := 'Circle';
+  DrawCircle();
+end;
+
+procedure TApp.Square1Click(Sender: TObject);
+begin
+  Square1.Checked := True;
+  Custom1.Checked := False;
+  resizingMode := 'Square';
+  App.BorderIcons := [biSystemMenu, biMinimize];
+  App.ClientWidth := App.ClientHeight;
+end;
+
+procedure TApp.Custom1Click(Sender: TObject);
+begin
+  Custom1.Checked := True;
+  Square1.Checked := False;
+  resizingMode := 'Custom';
+  App.BorderIcons := [biSystemMenu, biMinimize, biMaximize];
+end;
+
+procedure TApp.DrawRectangle();
 var
   centerX, centerY: real;
 begin
@@ -37,10 +86,10 @@ begin
   centerY := App.ClientHeight / 2;
 
   App.Canvas.Refresh;
-  App.Canvas.Brush.Color:=clFuchsia;
+  App.Canvas.Brush.Color := clFuchsia;
   App.Canvas.Rectangle(0, 0, App.ClientWidth, App.ClientHeight);
 
-  App.Canvas.Pen.Color:=clRed;
+  App.Canvas.Pen.Color := clRed;
 
   App.Canvas.MoveTo(round(centerX - size / 2), 0);
   App.Canvas.LineTo(round(centerX - size / 2), App.Height);
@@ -52,36 +101,76 @@ begin
   App.Canvas.MoveTo(0, round(centerY + size / 2));
   App.Canvas.LineTo(App.Width, round(centerY + size / 2));
 
-  App.Canvas.Pen.Color:=clBlack;
+  App.Canvas.Pen.Color := clBlack;
+end;
+
+procedure TApp.DrawCircle();
+var
+  centerX, centerY: real;
+begin
+  centerX := App.ClientWidth / 2;
+  centerY := App.ClientHeight / 2;
+
+  App.Canvas.Refresh;
+  App.Canvas.Brush.Color := clFuchsia;
+  App.Canvas.Rectangle(0, 0, App.ClientWidth, App.ClientHeight);
+
+  App.Canvas.Pen.Color := clRed;
+
+  App.Canvas.Ellipse(round(centerX - size / 2), round(centerY - size / 2),
+    round(centerX + size / 2), round(centerY + size / 2));
+
+  App.Canvas.MoveTo(round(centerX), 0);
+  App.Canvas.LineTo(round(centerX), App.Height);
+  App.Canvas.MoveTo(0, round(centerY));
+  App.Canvas.LineTo(App.Width, round(centerY));
+
+  App.Canvas.Pen.Color := clBlack;
 end;
 
 procedure TApp.FormCreate(Sender: TObject);
 begin
-  size:=10;
+  appMode := 'Rectangle';
+  size := 10;
 end;
 
 procedure TApp.FormMouseWheelDown(Sender: TObject; Shift: TShiftState;
   MousePos: TPoint; var Handled: Boolean);
 begin
   size := size - 1;
-  DrawCross();
+  if appMode = 'Rectangle' then
+    DrawRectangle()
+  else
+    DrawCircle();
 end;
 
 procedure TApp.FormMouseWheelUp(Sender: TObject; Shift: TShiftState;
   MousePos: TPoint; var Handled: Boolean);
 begin
   size := size + 1;
-  DrawCross();
+  if appMode = 'Rectangle' then
+    DrawRectangle()
+  else
+    DrawCircle();
 end;
 
 procedure TApp.FormPaint(Sender: TObject);
 begin
-  DrawCross();
+  if appMode = 'Rectangle' then
+    DrawRectangle()
+  else
+    DrawCircle();
 end;
 
 procedure TApp.FormResize(Sender: TObject);
 begin
-   DrawCross();
+  if resizingMode = 'Square' then
+    App.ClientWidth := App.ClientHeight;
+
+  if appMode = 'Rectangle' then
+    DrawRectangle()
+  else
+    DrawCircle();
 end;
 
 end.
